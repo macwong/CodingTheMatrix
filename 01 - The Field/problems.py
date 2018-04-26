002edb917bccd1a53dc5fa8344bb62c5632e1e08
Dave Mac <dave.a.mccormick@gmail.com>
2018-04-27 08:59:20 +1000

    Get all possible keys

--- 01 - The Field/problems.py
+++ 01 - The Field/problems.py
@@ -17,13 +17,136 @@ def plot_rotate():
 def plot_rotate_complex():
     plot({ 0.5 * 1j * z + (2 - 1j) for z in S }, 4)
 
-    
 
 #plot_imaginary()
 #plot_addition(1 + 2j)
 #plot_half()
 #plot_rotate()
-plot_rotate_complex()
+#plot_rotate_complex()
+
+from image import file2image
+
+def plot_image():
+    data = file2image("img01.png")
+    plot({ x + ((len(data) - y) * 1j) for x in range(len(data[0])) for y in range(len(data)) if sum(data[y][x]) / 3 < 120 }, 200)
+
+#plot_image()
+
+def f(z):
+    return { (s - z) for s in S }
+
+def f_2(z):
+    data = file2image("img01.png")
+    data_set = { x + ((len(data) - y) * 1j) for x in range(len(data[0])) for y in range(len(data)) if sum(data[y][x]) / 3 < 120 }
+    return { (s - z) for s in data_set }
+
+#mean_x = sum({ z.real for z in S }) / len(S)
+#mean_y = sum({ z.imag for z in S }) / len(S)
+#plot(f(mean_x + mean_y * 1j))
+
+#plot(f_2(90 + 80j), 200)
+
+from math import e, pi
+
+n_total = 20
+
+#points = { (e**((2 * pi * 1j) / n_total)) ** n for n in range(n_total) }
+
+#plot(points)
+
+
+#plot({ z * e ** (r * 1j) for z in S for r in { 0, pi / 4, pi / 2, 3 * pi / 4, pi, 5 * pi / 4, 3 * pi / 2, 7 * pi / 4, 2 * pi } })
+
+data = file2image("img01.png")
+data_set = { x + ((len(data) - y) * 1j) for x in range(len(data[0])) for y in range(len(data)) if sum(data[y][x]) / 3 < 120 }
+data_set = { z * e ** (r * 1j) for z in data_set for r in { 0, pi / 4, pi / 2, 3 * pi / 4, pi, 5 * pi / 4, 3 * pi / 2, 7 * pi / 4, 2 * pi } }
+
+#plot(data_set, 200)
+
+data_set = { ((x + ((len(data) - y) * 1j)) - 80 - 80j) * e ** (r * 1j) for x in range(len(data[0])) for y in range(len(data)) for r in { pi / 4 } if sum(data[y][x]) / 3 < 120 }
+#plot(data_set, 200)
+
+
+def one_time_pad(binary):
+    pad = []
+    
+    for val in range(32):
+        binary_key, _ = integer_to_binary(val)
+        
+        print(val, binary_key.zfill(5))
+#        for i in binary:
+#            num = int(i)
+#            
+#            for r in range(2):
+#                if r == 1 and num == 1:
+#                    pad[r] += "1"
+#                else:
+#                    pad[r] += "0"
+#                    
+#            for r in range(2):
+#                if r != num:
+#                    pad[r + 2] += "1"
+#                else:
+#                    pad[r + 2] += "0"
+#    print(pad)
+#    return pad
+
+def integer_to_binary(integer, exp = 0):
+    binary = ""    
+    
+    if (2 ** (exp + 1) <= integer):
+        binary, integer = integer_to_binary(integer, exp + 1)
+        
+    calc_exp = 2 ** exp
+    val = integer // calc_exp
+#    print(val)
+#    print(integer, calc_exp, val)
+    
+    if val > 0:
+        integer = integer - calc_exp
+        binary = binary + "1"        
+    else:
+        binary = binary + "0"
+    
+#    print(binary)
+
+    return binary, integer
+    
+def binary_to_integer(binary):
+    total = 0
+    
+    binary = one_time_pad(binary)[0]
+    
+    for i in range(len(binary)):
+        bit = binary[len(binary) - i - 1]
+        total += int(bit) * (2 ** i)
+            
+    return total
+
+def crack():
+    code = [ "10101", "00100", "10101", "01011", "11001", "00011", "01011", "10101", "00100", "11001", "11010" ]
+    
+    for arr in code:
+        t = binary_to_integer(arr)
+        
+        print(t, t + 65, chr(t + 65))
+        
+#crack()
+one_time_pad("10111")
+#binary, _ = integer_to_binary(23)
+#print(binary)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
